7b48c53bd3afc0bef75a7431a15c9f7df7b1f26c
mbildner <mbildner@icloud.com>
2019-09-25 13:02:25 -0400

    Update navbar for prices search

--- frontend/components/navbars/nav_bar.jsx
+++ frontend/components/navbars/nav_bar.jsx
@@ -5,87 +5,87 @@ import { Link, NavLink, Route } from 'react-router-dom';
 
 // renders nav header
 class NavBarComponent extends React.Component {
-    constructor(props) {
-        super(props);
-    
-        this.renderLoggedInLinks = this.renderLoggedInLinks.bind(this)
-        this.renderLoggedOutLinks = this.renderLoggedOutLinks.bind(this)
-        this.renderSessionLinks = this.renderSessionLinks.bind(this);
-    }
+	constructor(props) {
+		super(props);
 
-    renderLoggedInLinks() {
-        return (<>
-            <li>
-                <NavLink to="">Dashboard</NavLink>
-            </li>
-            <li>
-                <NavLink to="">Settings</NavLink>
-            </li>
-            <li >
-                <Link onClick={this.props.logoutUser} to="/" >Sign Out</Link>
-            </li>
-        </>
-        );
-    }
+		this.renderLoggedInLinks = this.renderLoggedInLinks.bind(this)
+		this.renderLoggedOutLinks = this.renderLoggedOutLinks.bind(this)
+		this.renderSessionLinks = this.renderSessionLinks.bind(this);
+	}
 
-    renderLoggedOutLinks() {
-        return (<>
-            <li>
-                <NavLink to='/login' activeClassName="nav-selected">Sign In</NavLink>
-            </li>
-            <li>
-                <NavLink to='/signup' activeClassName="nav-selected">Sign Up</NavLink>
-            </li>
-        </>
-        );
-    }
+	renderLoggedInLinks() {
+		return (<>
+			<li>
+				<NavLink to="">Dashboard</NavLink>
+			</li>
+			<li>
+				<NavLink to="">Settings</NavLink>
+			</li>
+			<li >
+				<Link onClick={this.props.logoutUser} to="/" >Sign Out</Link>
+			</li>
+		</>
+		);
+	}
 
-
-    renderSessionLinks() {
-        if (this.props.currentUser) {
-            return this.renderLoggedInLinks();
-        } else {
-            return this.renderLoggedOutLinks();
-        }
-    }
+	renderLoggedOutLinks() {
+		return (<>
+			<li>
+				<NavLink to='/login' activeClassName="nav-selected">Sign In</NavLink>
+			</li>
+			<li>
+				<NavLink to='/signup' activeClassName="nav-selected">Sign Up</NavLink>
+			</li>
+		</>
+		);
+	}
 
 
-    render() {
-        return (
-            <>
-                <div id="nav-container">
-                    <Link to='/' id="nav-logo">
-                        <img src="https://i.imgur.com/wLiW760.png" alt="coinspace-logo" />
-                    </Link>
-                    <div id="nav-sections">
-                        <div className="nav-section" id="nav-left">
-                            <ul>
-                                <li>
-                                    <NavLink activeClassName="nav-selected" to="">About</NavLink>
-                                </li>
-                                <li>
-                                    <NavLink activeClassName="nav-selected" to="">Products</NavLink>
-                                </li>
-                                <li>
-                                    <NavLink activeClassName="nav-selected" to="">Help</NavLink>
-                                </li>
-                                <li>
-                                    <NavLink activeClassName="nav-selected" to="">Price</NavLink>
-                                </li>
-                                
-                            </ul>
-                        </div>
+	renderSessionLinks() {
+		if (this.props.currentUser) {
+			return this.renderLoggedInLinks();
+		} else {
+			return this.renderLoggedOutLinks();
+		}
+	}
 
-                        <div className="nav-section" id="nav-right">
-                            <ul>
-                                {this.renderSessionLinks()}
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </>
-        );
-    }
+
+	render() {
+		return (
+			<>
+				<div id="nav-container">
+					<Link to='/' id="nav-logo">
+						<img src="https://i.imgur.com/wLiW760.png" alt="coinspace-logo" />
+					</Link>
+					<div id="nav-sections">
+						<div className="nav-section" id="nav-left">
+								<ul>
+									<li>
+										<NavLink activeClassName="nav-selected" to="">About</NavLink>
+									</li>
+									<li>
+										<NavLink activeClassName="nav-selected" to="">Products</NavLink>
+									</li>
+									<li>
+										<NavLink activeClassName="nav-selected" to="">Help</NavLink>
+									</li>
+									<li>
+										<NavLink activeClassName="nav-selected" to="/prices">Prices</NavLink>
+									</li>
+										
+								</ul>
+						</div>
+
+						<div className="nav-section" id="nav-right">
+							<ul>
+								{this.renderSessionLinks()}
+							</ul>
+						</div>
+					</div>
+				</div>
+			</>
+		);
+	}
 }
 
 
