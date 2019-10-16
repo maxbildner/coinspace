ba79144f6fbdcb4c932ad315847b095dba9d535b
mbildner <mbildner@icloud.com>
2019-10-16 19:53:13 -0400

    Create onChange functions to track local modal state

--- frontend/components/trading/tradeModal.jsx
+++ frontend/components/trading/tradeModal.jsx
@@ -10,21 +10,66 @@ import { connect } from 'react-redux';
 // connect(mapStateToProps, null)(TradeModal)
 
 
+// THIS COMPONENT IS A SUBCOMPONENT OF THE HOMEPAGE COMPONENT
 class TradeModal extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      symbol: this.props.symbol,
+      quantity: "Quantity"
+    }
+
+    this.handleBuy = this.handleBuy.bind(this);
+    this.handleSell = this.handleSell.bind(this);
+    this.onChangeSymbol = this.onChangeSymbol.bind(this);
+    this.onChangeQuantity = this.onChangeQuantity.bind(this);
   }
+
+
+  onChangeSymbol(event) {
+    this.setState({
+      symbol: event.target.value
+    });
+  }
+
+
+  onChangeQuantity(event) {
+    this.setState({
+      quantity: event.target.value
+    });
+  }
+
+
+  handleBuy() {
+    debugger
+  }
+
+
+  handleSell() {
+    debugger
+  }
+
 
   render() {
     // debugger
-    const { symbol, toggleModal } = this.props || {};
+    const { toggleModal } = this.props || {};
 
     return (
       <div id="modal-container">
         <span id="trading-modal-close-button" onClick={toggleModal}>&times;</span>
-        <input type="text" placeholder={symbol}/>
-        <input type="text" placeholder="Quantity"/>
-        <button>BUY</button> <button>SELL</button>
+        {/* <form action=""> */}
+          <input id="trading-input-symbol" 
+            type="text" 
+            placeholder={this.state.symbol}
+            onChange={this.onChangeSymbol}/>
+          <input id="trading-input-quantity" 
+            type="text" 
+            placeholder={this.state.quantity}
+            onChange={this.onChangeQuantity}/>
+          <button id="trading-modal-buy" onClick={this.handleBuy}>BUY</button>
+          <button id="trading-modal-buy" onClick={this.handleSell}>SELL</button>
+        {/* </form> */}
       </div>
     );
   }
