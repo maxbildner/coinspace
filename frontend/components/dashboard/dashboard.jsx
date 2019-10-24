081fdd941bede2d39ca681e1d796200604915967
mbildner <mbildner@icloud.com>
2019-10-24 13:35:02 -0400

    Write component did update method in parent component to get currency data and pass to wallets subcomponent

--- frontend/components/dashboard/dashboard.jsx
+++ frontend/components/dashboard/dashboard.jsx
@@ -3,17 +3,47 @@ import PortfolioChart from './portfolio_chart';
 import Watchlist from './watchlist';
 import PortfolioWallets from './portfolio_wallets';
 import Transactions from './transactions';
+import { fetchCurrencyInfo } from '../../util/prices_util';
+const SUPPORTED_CURRENCIES = ['BTC', 'ETH', 'XRP', 'BCH', 'LTC', 'EOS', 'XLM'];
 
 
 class Dashboard extends React.Component {
 	constructor(props) {
 		super(props);
+		// props == entire redux store
+
+		this.state = {
+			currentPrices: null,		// { BTC: 8000, ETH: 162... }
+		}
+
+		this.getCurrentPrices = this.getCurrentPrices.bind(this);
+	}
+
+
+	componentDidMount() {
+		if (this.state.currentPrices == null) {	
+			this.getCurrentPrices();
+		}
+	}
+
+
+	getCurrentPrices() {
+		fetchCurrencyInfo(SUPPORTED_CURRENCIES).then(
+			(response) => {
+				// response.RAW == {BTC: {USD: 7649.32, MKTCAP...}, ETH: {USD: 162.16, MKTCAP... }, ... }
+
+				return this.setState({
+					currentPrices: response.RAW
+				});
+			}
+		)
 	}
 
 
 	render() {
 		const userId = this.props.state.session.id;
 		const { cash_balance, portfolio } = this.props.state.entities.users[userId];
+		const currentPrices = this.state.currentPrices;
 		// debugger
 
 		return (
@@ -21,7 +51,10 @@ class Dashboard extends React.Component {
 				<h1>WELCOME!</h1>
 				<PortfolioChart/>
 				<Watchlist/>
-				<PortfolioWallets cashBalance={cash_balance} portfolio={portfolio}/>
+				<PortfolioWallets 
+					cashBalance={cash_balance} 
+					portfolio={portfolio} 
+					currentPrices={currentPrices}/>
 				<Transactions/>
 			</div>
 		);
--- frontend/components/dashboard/portfolio_wallets.jsx
+++ frontend/components/dashboard/portfolio_wallets.jsx
@@ -1,11 +1,54 @@
 import React from 'react';
+import { fetchCurrentPrices } from '../../util/prices_util';
+
+const SUPPORTED_CURRENCIES = ['BTC', 'ETH', 'XRP', 'BCH', 'LTC', 'EOS', 'XLM'];
 
 class PortfolioWallets extends React.Component {
   constructor(props) {
     super(props);
+    // props == { cashBalance, portfolio, currentPrices }
+    // currentPrices == {BTC: {USD: 7649.32, MKTCAP...}, ETH: {USD: 162.16, MKTCAP... }, ... }
+
+    // this.state = {
+    //   currentPrices: null,
+    // }
 
     this.sortPortfolio = this.sortPortfolio.bind(this);
     this.renderSortedRows = this.renderSortedRows.bind(this);
+    this.calculateAssetAllocation = this.calculateAssetAllocation.bind(this);
+  }
+
+
+  // componentDidMount() {
+  //   if (this.state.currentPrices == null) {
+  //     this.getCurrentPrices();
+  //   }
+  // }
+
+  // getCurrentPrices() {
+  //   fetchCurrentPrices(SUPPORTED_CURRENCIES).then(
+  //     (response) => {
+  //       debugger
+  //       // response == {BTC: {USD: 7649.32}, ETH: {USD: 162.16}, ... }
+  //       // return this.setState({
+  //       // });
+  //     }
+  //   );
+  // }
+
+
+  calculateAssetAllocation(sortedPortfolio) {     // returns new portfolio array of objects by adding %Allocation for each currency
+    // sortedPortfolio = [ { symbol: 'ETH', quantity: 30 }, { symbol: 'BTC', quantity: 1 }, { symbol: 'LTC', quantity: 0.5 } ]
+    
+    sortedPortfolio
+    // calculate value of each currency in USD 
+    // calculate total value in USD of entire portfolio (including cash balance)
+
+    
+
+    return portfolio.map( (currencyObj) => {
+
+    });
   }
 
 
@@ -27,21 +70,25 @@ class PortfolioWallets extends React.Component {
 
 
   renderSortedRows() {  // sort by asset allocation % high to low, default to coin mkt cap
-    supported_currencies = [ 'BTC', 'ETH', 'XRP', 'BCH', 'LTC', 'EOS', 'XLM'];
-    const sortedPortfolio = this.sortPortfolio();
+    let sortedPortfolio = this.sortPortfolio();
+    // sortedPortfolio = [ { symbol: 'ETH', quantity: 30 }, { symbol: 'BTC', quantity: 1 }, { symbol: 'LTC', quantity: 0.5 } ]
+    let portfolioAssetAllocation = this.calculateAssetAllocation(sortedPortfolio);
+
 
     return sortedPortfolio.map( currencyObj => {
       <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
+        <td>{currencyObj.symbol}</td>
+        <td>test</td>
+        <td>test</td>
+        <td>test</td>
       </tr>
     });
   }
 
 
   render() {
+    // debugger
+
     return (
       <table>
         <thead>
