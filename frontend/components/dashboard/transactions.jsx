260ab09b32b4fcdf588eb8e3609370e10094585d
mbildner <mbildner@icloud.com>
2019-10-25 12:53:50 -0400

    Set up basic html skeleton for transactions component

--- frontend/components/dashboard/transactions.jsx
+++ frontend/components/dashboard/transactions.jsx
@@ -3,12 +3,40 @@ import React from 'react';
 class Transactions extends React.Component {
   constructor(props) {
     super(props);
+
+    this.renderTransactions = this.renderTransactions.bind(this);
   }
+
+
+  renderTransactions() {
+    
+    return (
+      <tr className="transactions-tbody-tr">
+        <td>date</td>
+        <td>sold btc</td>
+        <td>1</td>
+      </tr>
+    );
+  }
+
+
 
   render() {
     return (
       <div id="transactions">
-        TRANSACTIONS
+        <table className="transactions-table">
+          <thead className="transactions-thead">
+            <tr className="transactions-thead-tr">
+              <th>Date</th>
+              <th>Transaction</th>
+              <th>Quantity</th>
+            </tr>
+          </thead>
+          <tbody className="transactions-tbody">
+            {this.renderTransactions()}
+          </tbody>
+        </table>
+        <div className="transactions-bottom">View your accounts</div>
       </div>
     );
   }
