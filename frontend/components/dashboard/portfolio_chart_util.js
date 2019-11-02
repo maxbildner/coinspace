519a21212c2ebf564a402897541aa2a5ea460a03
mbildner <mbildner@icloud.com>
2019-11-02 16:08:15 -0400

    Refactor calculate portfolioValues function and split with 3 helper functions

--- frontend/components/dashboard/portfolio_chart_util.js
+++ frontend/components/dashboard/portfolio_chart_util.js
@@ -1,62 +1,120 @@
-export const calculatePortfolioValues = (priceData, portfolio, cashBalance, transactions) => {
+export const calculatePortfolioValues = (pricesData, portfolio, cashBalance, transactions) => {
   // REUSABLE FUNCTION, priceData will always be populated!!! NOT EMPTY
   debugger
-  // priceData    == { 'BTC': [ {time:1569888000, close: 8326.24,...}, {}, ... ] }
+  // pricesData   == { 'BTC': [ {time:1569888000, close: 8326.24,...}, ... ], 'LTC':[], ... ] }
   // portfolio    == { 'BTC': 1 }
   // cashBalance  == 1871.57
   // transactions == [ { quantity, price, transaction_type, created_at, currency_symbol } ]
 
-  // grab first currency array in priceData object
-  let currencySymbol = Object.keys(priceData)[0];
-  // currencySymbol = 'BTC'
-  let currency = priceData[currencySymbol];
-  // currency = [ {time:1569888000, close: 8326.24,...}, {}, ... ]
+  // All currency keys in priceData
+  let currencySymbols = Object.keys(pricesData);
+  // currencySymbols = [ 'BTC', 'LTC' ]
 
-  let portfolioValues = [];                                             // to return
-  // 3- Loop through each price in any of the price arrays (all should be same length)
-  for (let i = 0; i < currency.length; i++) {
-    let priceObject = currency[i];
-    // priceObject = { time:1569888000, close: 8326.24,... }
+  // Grab first currency array in priceData object
+  let firstCurrencySymbol = currencySymbols[0];
+  // firstCurrencySymbol == 'BTC'
 
-    let portfolioValue = calculatePortfolioValue(priceObject);
-    
+  // Grab array at key of firstCurrencySymbol
+  let firstCurrency = pricesData[firstCurrencySymbol];
+  // firstCurrency == [ {time:1569888000, close: 8326.24,...}, {}, ... ]
+
+  let numDataPoints = firstCurrency.length;
+  // numDataPoints == 31    (monthly data)
+
+  // TO RETURN
+  let portfolioValues = [];                                                     
+  // => [ { time:1569801600, portfolioValue: 9000 }, { time:1569888000, portfolioValue: 9200 }, ... ]
+  
+
+  // 2) GET ALL PORTFOLIO QUANTITIES FOR EACH CURRENCY AT A PARTICULAR POINT IN TIME
+  // //=> { 'BTC':{price:8000, quantity 1}, 'USD':{price:1, quantity: 1000} }
+
+  // 3) CALCULATE PORTFOLIO VALUE AT A PARTICULAR POINT IN TIME
+
+
+  // Loop through each price in any of the price arrays (all should be same length)
+  // Calculate Portfolio Value at time t in this loop, and push to outside array (return this array later)
+  for (let t = 0; t < numDataPoints; t++) {
+    let symbol, priceAtTimeT, priceDataArray;
+
+    let time = firstCurrency[t][time];
+    // firstCurrency == [ {time:1569888000, close: 8326.24,...}, {}, ... ]
+    // time == 1569888000
+
+    // 1) GET ALL PRICES FOR EACH CURRENCY IN PORTFOLIO AT A PARTICULAR POINT IN TIME
+    let pricesAtTimeT = getPricesAtTimeT(t, pricesData);
+    // t = 0, pricesData = { 'BTC': [ {time:1569888000, close: 8326.24,...}, ... ], 'LTC':[], ... ] }
+    // pricesAtTimeT == { BTC:8326.24, LTC:164 }
+  
+    // Determine portfolio price/quantites quantities @time = t use helper function 1
+    // 2) HELPER FUNCTION 1 takes in TransactionHistory, and time         //=> returns object with keys of currencies, values of quantities @time = t
+    // (time, prices, transactionHistory)                                 //=> { 'BTC':{price:8000, quantity:1}, 'USD':{price:1, quantity:1000} }
+    let portfolioAtTimeT = getPortfolioAtTimeT(pricesAtTimeT, transactions);
+    // portfolioAtTimeT == { 'BTC':{price:8000, quantity:1}, 'USD':{price:1, quantity:1000} }
+
+    // Determine portfolio value @ time = t use helper function 2
+    // 3) HELPER FUNCTION 2 Takes in prices data at point in time, and quantities, and returns portfolio value at that point in time
+    // (time, price/quantity object)                                      // object of quantities/prices at a time t
+    // (1569801600, { 'BTC': { price:8000, quantity: 1 }, 'USD': { price:1, quantity: 1000 } })        
+    //                                                                    //=> { time:1569801600, portfolioValue: 9000 }
+    let portfolioValue = calculatePortfolioValueAtTimeT(time, portfolioAtTimeT);
+    // portfolioValue == { time:1569801600, portfolioValue: 9000 }
+
+    // push each portfolio value object to outside array (parseable by Recharts Library)
     portfolioValues.push(portfolioValue);
   }
 
   return portfolioValues;
-
-  // Determine portfolio @time = t use helper function 1
-  // Determine portfolio value @ time = t use helper function 2
-
-  // 4- HELPER FUNCTION 1) takes in TransactionHistory, and time        //=> returns object with keys of currencies, values of quantities @time = t
-  // (time, transactionHistory)                                         //=> { 'BTC':1, 'USD':1000 }
-
-  // 5- HELPER FUNCTION 2) Takes in price data at point in time, and quantities, and returns portfolio value at that point in time
-  // (time, price object, quantity object)                              // object with two key/val pairs
-  // (1569801600, {'BTC':8000, 'USD':1}, {'BTC':1, 'USD': 1000})        //=> { time:1569801600, portfolioValue: 9000 }
-
-  // Still inside loop, push each portfolio value object to outside array
-  //=> array of objects that is parseable by Recharts Library
-  //=> [ { time:1569801600, portfolioValue: 9000 }, { time:1569888000, portfolioValue: 9200 }, ... ]
-
-  // 6- Using output of Reusable function above, setState at corresponding time interval to output of this function
-
-  // ??? BELOW NOT NEEDED?
-  // Pass priceData obj, transactionHistory into function that returns an array of floats (portfolio values over that time period)
-  // priceData          == { 'BTC': [ {time:1569801600, close:8000 }, {}, ... ], 'ETH': [ {time:1569801600, close:160 }, {}, ... ] }
-  // transactionHistory == [ { time:1569801600 price: 8000, symbol: 'BTC', ... }, {}, ... ]
-  // (object, array of objects)                                         //=> 
-  // Loop through each price datapoint (array for each currency should all be same length)
-  // Determine quantity held of each currency for that point in time (use transaction history)- use helper function 1
-  // Determine portfolio value at that point in time (helper function 2)
-  // Push portfolio value object to array
+  // portfolioValues == [ { time:1569801600, portfolioValue: 9000 }, { time:1569888000, portfolioValue: 9200 }, ... ]
+  // Using return val of this function, setState at corresponding time interval outside this function
 }
 
 
-function calculateQuantity(time, transactionHistory) {
+
+
+
+// 1) GET ALL PRICES FOR EACH CURRENCY IN PORTFOLIO AT A PARTICULAR POINT IN TIME
+let pricesAtTimeT = getPricesAtTimeT(t, pricesData);
+    // t = 0, pricesData = { 'BTC': [ {time:1569888000, close: 8326.24,...}, ... ], 'LTC':[], ... ] }
+    // pricesAtTimeT == { BTC:8326.24, LTC:164 }
+
+// HELPER FUNCTIONS
+// 1) GETS ALL PRICES FOR EACH CURRENCY IN PORTFOLIO AT A PARTICULAR POINT IN TIME
+// takes in integer representing time point in time of time time period, and object of all prices over time for each currency
+// returns array of prices at time t
+// => [ {BTC:{time:1569888000, close: 8326.24,...}}, LTC:{time, close,...}, ... ]
+function getPricesAtTimeT(t, pricesData) {  
+  // t == 0                              
+  // pricesData   == { 'BTC': [ {time:1569888000, close: 8326.24,...}, ... ], 'LTC':[], ... ] }
+
+
+  // Grab price arrays for each currency in priceData
+  for (let j = 0; j < currencySymbols.length; j++) {
+    symbol = currencySymbols[j];
+    // i = 0, j = 0: symbol == 'BTC'
+
+    priceDataArray = pricesData[symbol];                                   // all price data for a single currency
+    // i = 0, j = 0: priceDataArray == [ {time:1569888000, close: 8326.24,...}, ... ]
+
+    priceAtTimeT = priceDataArray[i];
+    // i = 0, j = 0: priceAtTimeT == { time:1569888000, close: 8326.24,... }
+
+    let priceObject = {};
+    priceObject[symbol] = priceDataArray;
+    // priceObject == { 'BTC':[{time:1569888000, close: 8326.24,...}, ... ] }
+
+    pricesAtTimeT.push({});
+  }
+}
+
+
+
+function calculateQuantityAtTimeT(time, transactionHistory) {
 
 }
 
-function calculatePortfolioValue() {
+
+
+function calculatePortfolioValueAtTimeT() {
 
 }
