5d5742e1300a9ac698ab983e89c71951a61dfcd4
mbildner <mbildner@icloud.com>
2019-11-13 13:19:25 -0500

    Finish styling footer

--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -5,7 +5,8 @@ function Footer(props) {
     <>
       <div id="footer-wrapper">
         <div id="footer">
-          <span id="footer-name">Max Bildner</span> |
+          <div  id="footer-padding-top"></div>
+          <span id="footer-name">Max Bildner</span> | 
           <a id="footer-portfolio" href="https://maxbildner.github.io/"
             target="_blank">Portfolio</a>
         </div>
