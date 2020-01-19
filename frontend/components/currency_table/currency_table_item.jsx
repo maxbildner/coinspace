1873f0bbc61b419ead7efba0005e8b66d97adc80
mbildner <mbildner@icloud.com>
2020-01-19 19:13:56 +0800

    Enable live prices updates on search page

--- frontend/components/currency_table/currency_table_item.jsx
+++ frontend/components/currency_table/currency_table_item.jsx
@@ -19,8 +19,8 @@ class CurrencyTableItem extends React.Component {
 
 	componentDidMount() {
 		// Get new price every 10 seconds
-		setInterval(() => this.props.fetchCurrentPrice(this.props.symbol), 10000);
-		// this.props.fetchCurrentPrice(this.props.symbol);
+		// setInterval(() => this.props.fetchCurrentPrice(this.props.symbol), 10000);
+		this.props.fetchCurrentPrice(this.props.symbol);
 
 		fetchDescription(this.props.symbol).then(
 			(response) => {
--- frontend/components/prices/prices_row.jsx
+++ frontend/components/prices/prices_row.jsx
@@ -1,9 +1,47 @@
 import React from 'react';
-
+import { fetchCurrentPrice } from '../../util/prices_util';
 
 class PricesRow extends React.Component {
+  constructor(props) {
+    super(props);
+    // debugger
+
+    this.state = {
+      currentPrice: props.price,
+    };
+  }
+
+  componentDidMount() {
+    const { symbol } = this.props;
+
+    // Get new price every 10 seconds
+    setInterval(() => this.getCurrentPrice(symbol), 10000);
+  }
+
+  getCurrentPrice(symbol) {
+    // debugger
+    fetchCurrentPrice(symbol).then(
+      (response) => {
+        // debugger
+        // console.log('Fetched Current Price: ' + `${response.USD}`)						// for testing
+        // setTimeout(() => this.setState({ fading: false }), 500);
+
+        return this.setState({
+          // currentPrice: response.RAW.PRICE			// old for API that gets average price from multiple exchanges
+          currentPrice: formatValue(response.USD),
+        });
+      }
+    );
+  }
+
+
   render() {
-    const { nameToMap, symbol, price, percentChange, marketCap, logoPath } = this.props;
+    const { nameToMap, symbol, percentChange, marketCap, logoPath, price } = this.props;
+    const { currentPrice } = this.state;
+
+    let actualPrice;
+    (currentPrice) ? (actualPrice = currentPrice) : (actualPrice = price);
+    // debugger
 
     let isNegative;
     // if percentChange is negative, add a class to make text red, green otherwise
@@ -34,7 +72,8 @@ class PricesRow extends React.Component {
         </td>
         <td className="search-name">{nameToMap}</td>
         <td className="search-ticker">{symbol}</td>
-        <td className="search-price">{price}</td>
+        <td className="search-price">{actualPrice}</td>
+        {/* <td className="search-price">{price}</td> */}
         <td className={isNegative}>{percentChange}</td>
         <td className="search-marketCap">{marketCap}</td>
       </>
@@ -44,3 +83,14 @@ class PricesRow extends React.Component {
 
 export default PricesRow;
 
+
+
+
+// Helper Function
+// => returns string "10,526.94"
+function formatValue(value) {
+  // portfolioValue == 10527.94333333
+  if (value) {
+    return Number(value.toFixed(2)).toLocaleString();
+  }
+}
